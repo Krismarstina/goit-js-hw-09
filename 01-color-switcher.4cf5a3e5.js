668c3e47b85083a3a79169189f1670ed958dc1d9
Krismarstina <99805198+Krismarstina@users.noreply.github.com>
2022-09-07 10:53:48 +0000

    Deploying to gh-pages from @ Krismarstina/goit-js-hw-09@d0fa0170f13311553cb9efca44001ef6bc3d839b 🚀

---
+++ 01-color-switcher.4cf5a3e5.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){o=setInterval(d,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1,e.disabled=!0}));let o=null;function d(){n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}
+//# sourceMappingURL=01-color-switcher.4cf5a3e5.js.map
